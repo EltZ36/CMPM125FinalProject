0c650e1b02990d177d7179d90d77b396b6b2dbfd
aydenl3 <soloyeti41@gmail.com>
2024-12-04 13:34:03 -0800

    Summon limit implemented correctly, pooling implemented, summon behavior updated

--- Assets/Scripts/Summons/SummonManager.cs
+++ Assets/Scripts/Summons/SummonManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,7 +21,7 @@ public class SummonManager : MonoBehaviour
     private bool isSpawning = false;
     public int totalSummons;
     public AudioClip summonSound;
-
+    public List<GameObject> summonList;
     // Ensure that there's only one instance of the GameManager
     //awake: when this script is being loaded
     private void Awake()
@@ -35,7 +36,18 @@ public class SummonManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
+    private void Start()
+    {
+        summonList = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < totalSummons; i++)
+        {
+            Debug.Log("made summon" + i);
+            tmp = Instantiate(summonPrefab);
+            tmp.SetActive(false);
+            summonList.Add(tmp);
+        }
+    }
     public void StartGeneratingSummons()
     {
         if (!isSpawning)
@@ -62,12 +74,27 @@ public class SummonManager : MonoBehaviour
 
     void SpawnSummon()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(0, 2f), 0, 0);
+        //Vector3 spawnPosition = new Vector3(Random.Range(0, 2f), 0, 0);
         // Vector2 spawnPosition2 = new Vector2(Random.Range(-10f, 10f), Random.Range(-10f, 20f));
+        GameObject newsummon = GetSummon(summonList);
+        if (newsummon != null)
+        {            
+            newsummon.transform.position = Player.position + (transform.right);
+            Debug.Log(newsummon.transform.position);
+            newsummon.SetActive(true);
+        }
+    }
 
-        Instantiate(summonPrefab, Player.position + spawnPosition, Quaternion.identity);
-        // Instantiate(enemyPrefab2, spawnPosition2, Quaternion.identity);
-        Debug.Log("Summon spawned at: " + spawnPosition + " using Coroutine.");
+    public GameObject GetSummon(List<GameObject> list)
+    {
+        for (int i = 0; i < totalSummons; i++)
+        {
+            if (!list[i].activeInHierarchy)
+            {
+                return list[i];
+            }
+        }
+        return null;
     }
 }
 
